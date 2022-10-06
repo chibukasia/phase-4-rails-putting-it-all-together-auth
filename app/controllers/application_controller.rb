e0f52d0c1d0c23999008998d6631170f667eb391
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-06 14:48:38 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  wrap_parameters format: []
+  before_action :authorize
+  rescue_from ActiveRecord::RecordInvalid, with: :response_to_unprocessable_entity
+
+  # Private methods 
+
+  private 
+  def authorize 
+    render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+  end
+
+  # Method to catch invalid entries
+  def response_to_unprocessable_entity(err) 
+    render json: {errors: err.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
